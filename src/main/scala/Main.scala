d8a5a4b48ddaa50c59fa2b507ae02add632db59b
Josh Student <jrubin@berkeley.edu>
2021-01-07 17:58:45 -0800

    added many queries

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -13,21 +13,44 @@ object Main extends App {
     val statement = con.createStatement()
     var resultSet = statement.executeQuery("select * from geoeditors order by upper_bound desc limit 20")
 
-//    while (resultSet.next) {
-//      val foo = resultSet.getString("wiki_db")
-//      val bar = resultSet.getString("country")
-//      val bar = resultSet.getString("country")
-//      val bar = resultSet.getString("country")
-//      val bar = resultSet.getString("country")
-//      println(foo + " " + bar)
-//    }
-
     printResultSet(resultSet)
+    println("\n\n\n")
+    resultSet = statement.executeQuery("select * from pageviews order by count_views desc limit 20")
+    printResultSet(resultSet)
+    println("\n\n\n")
+    resultSet = statement.executeQuery("select * from pageviews where domain_code = \"en\" order by count_views desc limit 20")
+    printResultSet(resultSet)
+    println("\n\n\n")
+    resultSet = statement.executeQuery("select * from pageviews where domain_code = \"de\" order by count_views desc limit 20")
+    printResultSet(resultSet)
+    println("\n\n\n")
+    resultSet = statement.executeQuery("select * from pageviews where domain_code = \"es\" order by count_views desc limit 20")
+    printResultSet(resultSet)
+    println("\n\n\n")
+    resultSet = statement.executeQuery("select domain_code, sum(count_views) as total_views from pageviews group by domain_code order by total_views desc limit 20")
+    printResultSet(resultSet)
+    println("\n\n\n")
+    resultSet = statement.executeQuery("select * from pageviews order by count_views limit 20")
+    printResultSet(resultSet)
+    println("\n\n\n")
+    resultSet = statement.executeQuery("select wiki_db, sum(upper_bound) as upper_bound_sum from geoeditors group by wiki_db order by upper_bound_sum desc limit 20")
+    printResultSet(resultSet)
+    println("\n\n\n")
+    resultSet = statement.executeQuery("select wiki_db, sum((upper_bound + lower_bound) / 2) as avg_sum from geoeditors group by wiki_db order by avg_sum desc limit 20")
+    printResultSet(resultSet)
+    println("\n\n\n")
+    resultSet = statement.executeQuery("select * from geoeditors where wiki_db = \"enwiki\" order by upper_bound desc limit 20")
+    printResultSet(resultSet)
+    println("\n\n\n")
+    resultSet = statement.executeQuery("select * from geoeditors where wiki_db = \"eswiki\" order by upper_bound desc limit 20")
+    printResultSet(resultSet)
+
   } catch {
     case e: Exception => e.printStackTrace()
   }
 
   def printResultSet(resultSet: ResultSet): Unit = {
+    //https://stackoverflow.com/questions/24229442/print-the-data-in-resultset-along-with-column-names
     val rsmd = resultSet.getMetaData
     val columnsNumber = rsmd.getColumnCount
     while (resultSet.next) {
@@ -39,22 +62,6 @@ object Main extends App {
       System.out.println("")
     }
   }
-
-  import java.sql.ResultSetMetaData
-
-//  val resultSet = statement.executeQuery("SELECT * from foo")
-//  val rsmd = resultSet.getMetaData
-//  val columnsNumber = rsmd.getColumnCount
-//  while ( {
-//    resultSet.next
-//  }) {
-//    for (i <- 1 to columnsNumber) {
-//      if (i > 1) System.out.print(",  ")
-//      val columnValue = resultSet.getString(i)
-//      System.out.print(columnValue + " " + rsmd.getColumnName(i))
-//    }
-//    System.out.println("")
-//  }
 
   con.close();
 }
